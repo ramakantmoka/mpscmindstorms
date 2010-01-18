b10751794059c8086ef55318bd9c531ee0a51103
bernhard.merkle <bernhard.merkle@fccb064c-deb2-11de-95c9-073ad895b44c>
2010-01-18 23:23:25 +0000

    nxtOSEK 2_11 added (with some small patches for CR/LF endings)

---
+++ trunk/mpsproject/nxtOSEK/samples_c++/cpp/Helloworld/rxeflash.sh
@@ -0,0 +1,4 @@
+echo Executing NeXTTool to upload helloworld.rxe...
+ /cygdrive/C/ide/Cygwin/nexttool/NeXTTool.exe /COM=usb -download=helloworld.rxe
+ /cygdrive/C/ide/Cygwin/nexttool/NeXTTool.exe /COM=usb -listfiles=helloworld.rxe
+echo NeXTTool is terminated.
