b7fb87122aa1a60c43378410057b4109d6f58dc3
bernhard.merkle <bernhard.merkle@fccb064c-deb2-11de-95c9-073ad895b44c>
2010-02-07 20:00:11 +0000

    svn:ignore only *.bak *.iws

--- trunk/mpsproject/languages/nxtosek/source_gen/med/platform/nxtosek/rt/ecrobot_interface.h
+++ trunk/mpsproject/languages/nxtosek/source_gen/med/platform/nxtosek/rt/ecrobot_interface.h
@@ -14,11 +14,11 @@ void systick_wait_ms(int durationInMs);
 
 void ecrobot_status_monitor(char* msg);
 
-void ecrobot_set_light_sensor_active(SENSOR_PORT_T port_id);
+void ecrobot_set_light_sensor_active(int port_id);
 
-int ecrobot_get_light_sensor(SENSOR_PORT_T port_id);
+int ecrobot_get_light_sensor(int port_id);
 
-void ecrobot_set_light_sensor_inactive(SENSOR_PORT_T port_id);
+void ecrobot_set_light_sensor_inactive(int port_id);
 
 void display_goto_xy(int x, int y);
 
@@ -26,18 +26,18 @@ void display_string(char* str);
 
 void display_int(int val, int anotherArgumentIDontKnowWhatItMeans);
 
-void nxt_motor_set_speed(MOTOR_PORT_T motor_port, int speed, int direction);
+void nxt_motor_set_speed(int motor_port, int speed, int direction);
 
 void display_update(void);
-enumSENSOR_PORT_T{
+enum SENSOR_PORT_T {
 NXT_PORT_S1
-,NXT_PORT_S2
-,NXT_PORT_S3
-,NXT_PORT_S4
+, NXT_PORT_S2
+, NXT_PORT_S3
+, NXT_PORT_S4
 };
-enumMOTOR_PORT_T{
+enum MOTOR_PORT_T {
 NXT_PORT_A
-,NXT_PORT_B
-,NXT_PORT_C
+, NXT_PORT_B
+, NXT_PORT_C
 };
 #endif
