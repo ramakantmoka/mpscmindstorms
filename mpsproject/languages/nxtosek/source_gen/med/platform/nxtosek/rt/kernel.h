dda540b0190a310889557d0b81cafd50b87e9b78
bernhard.merkle <bernhard.merkle@fccb064c-deb2-11de-95c9-073ad895b44c>
2010-02-07 20:00:11 +0000

    svn:ignore only *.bak *.iws

---
+++ mpsproject/languages/nxtosek/source_gen/med/platform/nxtosek/rt/kernel.h
@@ -0,0 +1,17 @@
+#ifndef NXTOSEKKERNEL
+#define NXTOSEKKERNEL
+/* 
+---------------------------------------------------------------
+   header generated from modularEmbeddedDSL via MPS 
+   resource NxtOsekKernel
+--------------------------------------------------------------- 
+*/
+
+// custom includes for header
+
+
+void TerminateTask(void);
+
+void ShutdownOS(StatusType status);
+typedef int StatusType;
+#endif
