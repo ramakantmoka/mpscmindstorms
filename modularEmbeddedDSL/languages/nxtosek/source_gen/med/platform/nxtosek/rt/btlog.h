3cc910f9c4ee58f7476e0ab275a426ad50160ad3
bernhard.merkle <bernhard.merkle@fccb064c-deb2-11de-95c9-073ad895b44c>
2010-03-12 10:45:43 +0000

    adding missing files after regeneration

---
+++ modularEmbeddedDSL/languages/nxtosek/source_gen/med/platform/nxtosek/rt/btlog.h
@@ -0,0 +1,14 @@
+#ifndef NXTOSEKLOGGING
+#define NXTOSEKLOGGING
+/* 
+---------------------------------------------------------------
+   header generated from modularEmbeddedDSL via MPS 
+   resource NxtOsekLogging
+--------------------------------------------------------------- 
+*/
+
+// custom includes for header
+
+
+void btlog(char* data);
+#endif
