de6e41792ac1108b8fa728841531273ac6fef28c
bernhard.merkle <bernhard.merkle@fccb064c-deb2-11de-95c9-073ad895b44c>
2010-05-14 21:59:56 +0000

    generated _samples: leads to add new files... note: - MessageComponents - modularEmbeddedDSL.sandbox can not be generated as - util.* can not be generated ATM.

--- modularEmbeddedDSL/solutions/LineFollower/source_gen/lfmain/LineFollower.c
+++ modularEmbeddedDSL/solutions/LineFollower/source_gen/lfmain/LineFollower.c
@@ -9,10 +9,10 @@
 #include "include/LineFollower.h"
 
 // used resources
+#include "avgutil.h"
 #include "bitdata.h"
-#include "kernel.h"
 #include "ecrobot_interface.h"
-#include "safeutil.h"
+#include "kernel.h"
 
 // custom includes
 #include "kernel.h"
@@ -20,7 +20,10 @@
 #include "stdint.h"
 #include "stdint.h"
 
+int LineFollower_main_currentSonar = 250;
 int LineFollower_main_linefollower_currentstate = STATE_INITIALIZING;
+int LineFollower_main_currentSonar_history[10] = { 250, 250, 250, 250, 250, 250, 250, 250, 250, 250 };
+int LineFollower_main_currentSonar_index = 0;
 
 void LineFollower_main_linefollower_execute(int event) {
         
@@ -40,10 +43,112 @@ void LineFollower_main_linefollower_execute(int event) {
 
     } // end if 
 
+        
+    if ( LineFollower_main_linefollower_currentstate == STATE_PAUSED) {
+                
+      if ( event == EVENT_UNBLOCKED) {
+                      
+        if ( 1) {
+                  LineFollower_main_linefollower_currentstate = STATE_RUNNING;
+          return ;
+
+        } // end if 
+
+
+      } // end if 
+
+
+    } // end if 
+
+        
+    if ( LineFollower_main_linefollower_currentstate == STATE_RUNNING) {
+                
+      if ( event == EVENT_BLOCKED) {
+                      
+        if ( 1) {
+                  LineFollower_main_linefollower_currentstate = STATE_PAUSED;
+                      int16_t i = 1;
+
+          return ;
+
+        } // end if 
+
+
+      } // end if 
+
+            
+      if ( event == EVENT_BUMPED) {
+                      
+        if ( 1) {
+                  LineFollower_main_linefollower_currentstate = STATE_CRASH;
+          return ;
+
+        } // end if 
+
+
+      } // end if 
+
+            
+      if ( event == EVENT_UNBLOCKED) {
+                      
+        if ( 1) {
+                  LineFollower_main_linefollower_currentstate = STATE_CRASH;
+          return ;
+
+        } // end if 
+
+
+      } // end if 
+
+
+    } // end if 
+
+        
+    if ( LineFollower_main_linefollower_currentstate == STATE_CRASH) {
+                
+      if ( event == EVENT_BLOCKED) {
+                      
+        if ( 1) {
+                  LineFollower_main_linefollower_currentstate = STATE_INITIALIZING;
+          return ;
+
+        } // end if 
+
+
+      } // end if 
+
+            
+      if ( event == EVENT_UNBLOCKED) {
+                      
+        if ( 1) {
+                  LineFollower_main_linefollower_currentstate = STATE_CRASH;
+          return ;
+
+        } // end if 
+
+
+      } // end if 
+
+            
+      if ( event == EVENT_BUMPED) {
+                      
+        if ( 1) {
+                  LineFollower_main_linefollower_currentstate = STATE_INITIALIZING;
+          return ;
+
+        } // end if 
+
+
+      } // end if 
+
+
+    } // end if 
+
 }
 
 void ecrobot_device_initialize(){
     ecrobot_set_light_sensor_active (NXT_PORT_S1 );
+    ecrobot_init_sonar_sensor (NXT_PORT_S2 );
     LineFollower_main_linefollower_execute (EVENT_INITIALIZED );
 
 }
@@ -53,11 +158,26 @@ void ecrobot_device_terminate(){
 
 }
 
+TASK(LineFollower_main_sonartask){
+    LineFollower_main_currentSonar = calcAvgInt (LineFollower_main_currentSonar_history, &LineFollower_main_currentSonar_index, ecrobot_get_sonar_sensor (NXT_PORT_S2 ), 10 );
+    TerminateTask(); // automatically added by platform.osek:addTermianateTask
+
+}
+
 TASK(LineFollower_main_run){
                 
       if ( LineFollower_main_linefollower_currentstate == STATE_RUNNING) {
-              int32_t light = 0;
-        light = isValidInt32 (ecrobot_get_light_sensor (NXT_PORT_S1 ) );
+              int8_t bump = 0;
+        bump = ecrobot_get_touch_sensor (NXT_PORT_S3 );
+                
+        if ( bump == 1) {
+                  LineFollower_main_linefollower_execute (EVENT_BUMPED );
+          TerminateTask();
+
+        } // end if 
+
+        int32_t light = 0;
+        light = ecrobot_get_light_sensor (NXT_PORT_S1 );
                 
         if ( (light < (((500 + 700)) / (2)))) {
                   LineFollower_main_updateMotorSettings (20, 40 );
@@ -69,6 +189,28 @@ TASK(LineFollower_main_run){
 
         } // end if 
 
+        TerminateTask();
+        return ;
+
+      } // end if 
+
+            
+      if ( LineFollower_main_linefollower_currentstate == STATE_PAUSED) {
+              LineFollower_main_updateMotorSettings (0, 0 );
+                
+        if ( (LineFollower_main_currentSonar < 255)) {
+                  LineFollower_main_linefollower_execute (EVENT_UNBLOCKED );
+
+        } // end if 
+
+        TerminateTask();
+        return ;
+
+      } // end if 
+
+            
+      if ( LineFollower_main_linefollower_currentstate == STATE_CRASH) {
+              LineFollower_main_updateMotorSettings (0, 0 );
         TerminateTask();
         return ;
 
--- modularEmbeddedDSL/solutions/LineFollower/source_gen/lfmain/makefile.mak
+++ modularEmbeddedDSL/solutions/LineFollower/source_gen/lfmain/makefile.mak
@@ -1,8 +1,8 @@
 TARGET = LineFollowerOSEK_OSEK
 TARGET_SOURCES = LineFollower.c \
 	../../../osek_mps_lib/bitdata.c \
-	../../../osek_mps_lib/safeutil.c \
-	../../../osek_mps_lib/safeutil.c
+	../../../osek_mps_lib/avgutil.c \
+	../../../osek_mps_lib/avgutil.c
 TOPPERS_OSEK_OIL_SOURCE = ./LineFollowerOSEK.oil
  
 O_PATH ?= build
